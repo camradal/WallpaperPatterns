c676837a155f9739edc0356aa0e3da05e96a62e2
Alexey Kalinin <camradal@yahoo.com>
2013-09-07 13:25:31 -0700

    Create a hub Windows 8.1 application

---
+++ WallpaperPatterns.Store81/Bootstrap/FilePluginBootstrap.cs
@@ -0,0 +1,9 @@
+using Cirrious.CrossCore.Plugins;
+
+namespace WallpaperPatterns.Store81.Bootstrap
+{
+    public class FilePluginBootstrap
+        : MvxPluginBootstrapAction<Cirrious.MvvmCross.Plugins.File.PluginLoader>
+    {
+    }
+}
