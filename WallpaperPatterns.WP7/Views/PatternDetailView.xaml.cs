e2575239fb417924c848eaacc0ff9570375330f2
Alexey Kalinin <camradal@yahoo.com>
2014-01-07 23:15:13 -0800

    First set bg implementation

--- WallpaperPatterns.WP7/Views/PatternDetailView.xaml.cs
+++ WallpaperPatterns.WP7/Views/PatternDetailView.xaml.cs
@@ -1,18 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.IO.IsolatedStorage;
-using System.Linq;
-using System.Net;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using Microsoft.Phone.Controls;
-using Microsoft.Phone.Shell;
 using Microsoft.Xna.Framework.Media;
-using WallpaperPatterns.Core.Service;
+using Windows.Phone.System.UserProfile;
 using WallpaperPatterns.Core.ViewModels;
 
 namespace WallpaperPatterns.WP7.Views
@@ -41,17 +35,57 @@ namespace WallpaperPatterns.WP7.Views
 
         private void ApplicationBarIconButton_Click_Download(object sender, EventArgs e)
         {
-            var viewModel = (PatternDetailViewModel)ViewModel;
+            var viewModel = (PatternDetailViewModel) ViewModel;
             string title = viewModel.Title;
             string uriString = viewModel.ImageUrl;
 
-            var uriSource = new Uri(uriString, UriKind.Absolute);
             int targetWidth = 480;
             int targetHeight = 800;
+            
+            WriteableBitmap writeableBitmap = GetBitmap(uriString, targetWidth, targetHeight);
+            SaveImageToMediaLibrary(writeableBitmap, targetWidth, targetHeight, title);
+        }
+
+        private async void ApplicationBarIconButton_Click_Wallpaper(object sender, EventArgs e)
+        {
+            var viewModel = (PatternDetailViewModel)ViewModel;
+            string title = viewModel.Title;
+            string uriString = viewModel.ImageUrl;
+            string fileName = title + ".jpeg";
+
+            int targetWidth = 480;
+            int targetHeight = 800;
+
+            WriteableBitmap writeableBitmap = GetBitmap(uriString, targetWidth, targetHeight);
+            SaveImageToIsolatedStorage(writeableBitmap, targetWidth, targetHeight, fileName);
+
+            if (!LockScreenManager.IsProvidedByCurrentApplication)
+            {
+                LockScreenRequestResult result = await LockScreenManager.RequestAccessAsync();
+                if (result == LockScreenRequestResult.Granted)
+                {
+                    SetAsWallpaper(fileName);
+                }
+            }
+            else
+            {
+                SetAsWallpaper(fileName);
+            }
+        }
+
+        private void SetAsWallpaper(string filename)
+        {
+            string realPath = "ms-appdata:///local/" + filename;
+            LockScreen.SetImageUri(new Uri(realPath, UriKind.Absolute));
+        }
+
+        private WriteableBitmap GetBitmap(string uriString, int targetWidth, int targetHeight)
+        {
+            var uriSource = new Uri(uriString, UriKind.Absolute);
             var imageSource = new BitmapImage(uriSource) { CreateOptions = BitmapCreateOptions.None };
             var canvas = GetCanvas(imageSource, targetWidth, targetHeight);
             var writeableBitmap = new WriteableBitmap(canvas, null);
-            SaveImageToMediaLibrary(writeableBitmap, targetWidth, targetHeight, title);
+            return writeableBitmap;
         }
 
         private TileCanvas GetCanvas(BitmapImage imageSource, int targetWidth, int targetHeight)
@@ -85,6 +119,22 @@ namespace WallpaperPatterns.WP7.Views
                 }
                 stream.Seek(0, SeekOrigin.Begin);
                 mediaLibrary.SavePicture(title, stream);
+            }
+        }
+
+        private static void SaveImageToIsolatedStorage(WriteableBitmap bitmap, int targetWidth, int targetHeight, string fileName)
+        {
+            using (IsolatedStorageFile storage = IsolatedStorageFile.GetUserStoreForApplication())
+            {
+                if (storage.FileExists(fileName))
+                {
+                    storage.DeleteFile(fileName);
+                }
+
+                using (IsolatedStorageFileStream stream = storage.CreateFile(fileName))
+                {
+                    bitmap.SaveJpeg(stream, targetWidth, targetHeight, 0, 100);
+                }
             }
         }
     }
